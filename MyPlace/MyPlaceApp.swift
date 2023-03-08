791e91504ec7b5144a26b8f20085717bfce1b865
jayastronomic <juliansmith94@hotmail.com>
2023-03-08 14:49:22 -0600

    Initial Commit

---
+++ MyPlace/MyPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlaceApp.swift
+//  MyPlace
+//
+//  Created by Julian Smith on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MyPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
